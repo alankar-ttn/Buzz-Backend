fb03b4b4fa338f553f5da8aea85f0b29e8fbdd3b
alankar-ttn <alankar.saxena@tothenew.com>
2022-04-13 15:32:50 +0530

    FIXES

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,18 @@ const admin = require("../config/firebase-config");
 const { User } = require("../models/user");
 
 module.exports = async function (req, res, next) {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    const user = await User.findOne({
-        uid: decodedToken.uid
-    });
-    if (!user) {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        const user = await User.findOne({
+            uid: decodedToken.uid
+        });
+        if (!user) {
+            return res.status(401).send("Unauthorized");
+        }
+        req.user = user;
+        next(); 
+    } catch (error) {
         return res.status(401).send("Unauthorized");
     }
-    req.user = user;
-    next(); 
 };
