e5a5256fb363e144f0ceb009bb01b1f1ddd872d7
alankar-ttn <alankar.saxena@tothenew.com>
2022-04-28 14:47:01 +0530

    added port env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("./startup/routes")(app)
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.info(`Listening on port ${port}...`));
 
 module.exports = server;
